e863ac1a893179b02c95df259ece652dd83aab0b
Evan Bailey <baileyevang@gmail.com>
2016-06-04 01:21:36 -0700

    Added binding and styles for interface buttons

--- overwatch-suggest.js
+++ overwatch-suggest.js
@@ -1,5 +1,17 @@
-var ViewModel = function() {
-    this.Heroes = [
+var HeroViewModel = function (hero) {
+    this.Id = hero.Id;
+
+    this.AddEnemy = function (enemy) {
+        console.log("adding " + JSON.stringify(enemy) + "as enemy");
+    }
+
+    this.AddAlly = function (ally) {
+        console.log("adding " + JSON.stringify(ally) + "as ally");
+    }
+}
+
+var ViewModel = function () {
+    var Heroes = [
         {
             Id: "Winston"
         },
@@ -12,7 +24,11 @@ var ViewModel = function() {
         {
             Id: "Soldier: 76"
         },
-    ] 
+    ];
+
+    this.HeroViewModels = Heroes.map(function (hero) {
+        return new HeroViewModel(hero);
+    });
 };
 
 $(document).ready(function() {
